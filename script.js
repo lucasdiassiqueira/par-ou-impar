6e0f7c22980c31db05a803874511dd3a5c524b33
lucasdiassiqueira <lucas.dias.siqueira@escola.pr.gov.br>
2025-03-23 15:22:38 -0300

    9 js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-document.getElementById('generate').addEventListener('click', function() {
+const button = document.getElementById('generate');
+
+button.addEventListener('click', function() {
     const min = parseInt(document.getElementById('min').value);
     const max = parseInt(document.getElementById('max').value);
-    const resultElement = document.getElementById('result').querySelector('span');
 
-    // Verifica se os valores são válidos
-    if (isNaN(min) || isNaN(max) || min > max) {
-        resultElement.textContent = 'Valores inválidos!';
-        resultElement.style.color = '#e74c3c'; // Vermelho para erro
-        return;
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (isNaN(result)) {
+        result = 'Valor inválido';
     }
 
-    // Gera o número aleatório
-    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    resultElement.textContent = randomNumber;
-    resultElement.style.color = '#2b4bd9'; // Cor do número gerado
+    document.querySelector('#result > span').textContent = result;
 });
