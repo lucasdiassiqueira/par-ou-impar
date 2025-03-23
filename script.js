13d7b2ff92f280329ed7e65e0ae4e12f75b9e79d
lucasdiassiqueira <lucas.dias.siqueira@escola.pr.gov.br>
2025-03-23 16:53:01 -0300

    1.4

--- script.js
+++ script.js
@@ -12,31 +12,31 @@ button.addEventListener('click', function() {
 
     document.querySelector('#result > span').textContent = result;
 
-    // Exibe os botões para escolher ímpar ou par
-    gameButtons.style.display = 'block';
-    gameResult.style.display = 'none'; // Esconde a mensagem de resultado anterior
-});
-
-document.getElementById('parButton').addEventListener('click', function() {
-    checkGuess('par');
-});
-
-document.getElementById('imparButton').addEventListener('click', function() {
-    checkGuess('impar');
-});
-
-function checkGuess(guess) {
-    // Verifica se o número gerado é par ou ímpar
-    const isEven = generatedNumber % 2 === 0;
-
-    // Verifica se o chute está correto
-    if ((guess === 'par' && isEven) || (guess === 'impar' && !isEven)) {
-        gameMessage.textContent = 'Você acertou!';
-    } else {
-        gameMessage.textContent = 'Você errou!';
-    }
-
-    // Exibe o resultado
-    gameResult.style.display = 'block';
-    gameButtons.style.display = 'none'; // Esconde os botões de jogo
-}
+     // Exibe os botões para escolher ímpar ou par
+     gameButtons.style.display = 'block';
+     gameResult.style.display = 'none'; // Esconde a mensagem de resultado anterior
+ });
+ 
+ document.getElementById('parButton').addEventListener('click', function() {
+     checkGuess('par');
+ });
+ 
+ document.getElementById('imparButton').addEventListener('click', function() {
+     checkGuess('impar');
+ });
+ 
+ function checkGuess(guess) {
+     // Verifica se o número gerado é par ou ímpar
+     const isEven = generatedNumber % 2 === 0;
+ 
+     // Verifica se o chute está correto
+     if ((guess === 'par' && isEven) || (guess === 'impar' && !isEven)) {
+         gameMessage.textContent = 'Você acertou!';
+     } else {
+         gameMessage.textContent = 'Você errou!';
+     }
+ 
+     // Exibe o resultado
+     gameResult.style.display = 'block';
+     gameButtons.style.display = 'none'; // Esconde os botões de jogo
+ }
