be2d2517bd21f8b5e10f8c9daf7e8d3e2d6a0b7a
lucasdiassiqueira <lucas.dias.siqueira@escola.pr.gov.br>
2025-03-24 16:46:09 -0300

    3.1

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const button = document.getElementById('generate');
 
 button.addEventListener('click', function() {
-    let generatedNumber; // Declara a variável dentro do escopo da função
-
     const min = parseInt(document.getElementById('min').value);
     const max = parseInt(document.getElementById('max').value);
 
-    if (!isNaN(min) && !isNaN(max) && min <= max) {
-        generatedNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    } else {
-        generatedNumber = 'Valor inválido';
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (isNaN(result)) {
+        result = 'Valor inválido';
     }
 
-    document.querySelector('#result > span').textContent = generatedNumber;
-    console.log("Número gerado:", generatedNumber); // Verifica se a variável está funcionando
+    document.querySelector('#result > span').textContent = result;
 });
+
 
 
 
