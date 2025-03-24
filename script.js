1ed8a4fbb641a3d23f408c9542649d0271bb4b19
lucasdiassiqueira <lucas.dias.siqueira@escola.pr.gov.br>
2025-03-24 19:36:00 -0300

    4.5

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ let currentNumber;
 function generateNumber() {
     currentNumber = Math.floor(Math.random() * 100);
     document.getElementById("randomNumber").textContent = currentNumber;
+    document.getElementById("resultMessage").textContent = "";
 }
 
 function checkGuess(choice) {
     const isEven = currentNumber % 2 === 0;
+    const resultMessage = document.getElementById("resultMessage");
     if ((choice === 'par' && isEven) || (choice === 'impar' && !isEven)) {
-        alert("Você acertou!");
+        resultMessage.textContent = "Você acertou!";
+        resultMessage.style.color = "green";
     } else {
-        alert("Você errou! Tente novamente.");
+        resultMessage.textContent = "Você errou! Tente novamente.";
+        resultMessage.style.color = "red";
     }
     generateNumber();
 }
 
 // Gera um número aleatório ao iniciar
 generateNumber();
-
