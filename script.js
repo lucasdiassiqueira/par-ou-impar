2a7600805e7cae3d757e7111b8a81e94a9bd9508
lucasdiassiqueira <lucas.dias.siqueira@escola.pr.gov.br>
2025-03-24 16:12:46 -0300

    2.4

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ button.addEventListener('click', function() {
 
     document.querySelector('#result > span').textContent = result;
 });
+
+function checkGuess(choice) {
+    if (generatedNumber === null) {
+        gameMessage.textContent = 'Gere um número primeiro!';
+    } else {
+        const isEven = generatedNumber % 2 === 0;
+        if ((choice === 'par' && isEven) || (choice === 'impar' && !isEven)) {
+            gameMessage.textContent = 'Você acertou!';
+            gameMessage.style.color = 'green';
+        } else {
+            gameMessage.textContent = 'Você errou!';
+            gameMessage.style.color = 'red';
+        }
+    }
+    gameResult.style.display = 'block';
+}
+
+parButton.addEventListener('click', () => checkGuess('par'));
+imparButton.addEventListener('click', () => checkGuess('impar'));
