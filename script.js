72cf29070172a0c01709771d01aedecc786a0cc1
lucasdiassiqueira <lucas.dias.siqueira@escola.pr.gov.br>
2025-03-24 16:39:33 -0300

    3.0

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const button = document.getElementById('generate');
-let generatedNumber; // Variável global para armazenar o número gerado
 
 button.addEventListener('click', function() {
+    let generatedNumber; // Declara a variável dentro do escopo da função
+
     const min = parseInt(document.getElementById('min').value);
     const max = parseInt(document.getElementById('max').value);
 
-    generatedNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    if (isNaN(generatedNumber)) {
+    if (!isNaN(min) && !isNaN(max) && min <= max) {
+        generatedNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    } else {
         generatedNumber = 'Valor inválido';
     }
 
     document.querySelector('#result > span').textContent = generatedNumber;
-
-    console.log("Número gerado:", generatedNumber); // Agora você pode acessar fora do evento
+    console.log("Número gerado:", generatedNumber); // Verifica se a variável está funcionando
 });
 
 
